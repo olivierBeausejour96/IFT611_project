94a2a4a4dad7df3f406ff60c047cee3e52d916ba
retardedpunk <beao3002@usherbrooke.ca>
2019-03-10 22:00:04 -0400

    not much but some lots of fun with doc tests

--- client/src/bin/main.rs
+++ client/src/bin/main.rs
@@ -1,3 +1,13 @@
+extern crate ift611_user;
+
+use ift611_user as us;
+use ift611_user::shared as sh;
+
 fn main() {
-    println!("Hello world!");
+    println!("main Hello world");
+    us::execute();
+
+    let d = sh::Data { high: 32, low: 32, opening: 32, closing: 32 };
+
+    println!("{:?}", d);
 }
